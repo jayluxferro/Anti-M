3ae42ae49cc70b43536d84d8bd55744c5b0c4b7b
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2020-10-12 18:49:08 +0000

    Initial Commit

---
+++ Mosquito Repellent/Mosquito_RepellentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mosquito_RepellentApp.swift
+//  Mosquito Repellent
+//
+//  Created by Jay on 12/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Mosquito_RepellentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
